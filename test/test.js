7a850012b4b366bb29694eaf107a77d933963818
AqilCont <32044067+AqilCont@users.noreply.github.com>
2018-10-11 11:57:19 -0400

    Updated tests for more coverage

--- test/test.js
+++ test/test.js
@@ -4,10 +4,22 @@
 
 const expect = require("chai").expect;
 const pack = require("../main.js");
+const functions = require("../stuff/functions.js");
+const events = require("../stuff/events.js");
+const trivia = require("../stuff/trivia.js");
 
 describe("#rfasqls", function() {
-  it("should check if check works properly", function() {
-    var result = pack.Functions.check(["hello", "jdsajdlask", 121212], "string");
-    expect(result).to.equal(false);
+  it("should check if check works properly and if every value from functions is a function", function() {
+    var result = functions.check(pack.functions, "function");
+    expect(result).to.equal(true);
+  })
+  it("should give me an object containing on and emit functions plus an array of events", function() {
+    var result = functions.check(new events(), ["on", "emit", "events"]);
+    expect(result).to.equal(true);
+  });
+  it("should give me an object containing a question, answers, and other crap", async function() {
+    const question = await trivia.getQuestion();
+    var result = question.response_code === 0 && functions.check(question.result, ["question", "correct_answer", "incorrect_answers", "type", "difficulty", "category"]);
+    expect(result).to.equal(true);
   })
 })
